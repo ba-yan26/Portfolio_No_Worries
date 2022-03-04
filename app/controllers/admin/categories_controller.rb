839d81ae5dc9fd9eff77e8b926232da0cb40c596
ba-yan26 <ladkol2626@gmail.com>
2022-03-04 05:00:57 +0000

    AddCategory

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,28 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories = Category.all
+    @category = Category.new
   end
 
-  def new
+  def create
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to admin_categories_path
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    category = Category.find(params[:id])
+    category.update(category_params)
+    redirect_to admin_categories_path
+  end
+  
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -1,10 +1,33 @@
 class Public::RoomsController < ApplicationController
   def new
+    @room = Room.new
+    @categories = Category.all
+  end
+
+  def create
+    @room = Room.new(room_params)
+    @room.end_user_id = current_end_user.id
+    @room.save
+    redirect_to room_path(@room)
   end
 
   def index
+    @rooms = Room.all
+    @categories = Category.all
   end
 
   def show
+    @room = Room.find(params[:id])
+    @categories = Category.all
+  end
+
+  def destory
+
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:title, :body, :resolution_status, :category_id)
   end
 end
